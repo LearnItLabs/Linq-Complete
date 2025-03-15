6fab0a4ec899b0178cd78d26a7fe2d778bd53a38
Walt Ritscher <waltritscher@outlook.com>
2025-03-15 12:37:08 -0700

    update sqllite db and data depot examples

--- code/2-LinqToDB/VisualStudio/DataDepot/Program.cs
+++ code/2-LinqToDB/VisualStudio/DataDepot/Program.cs
@@ -1,21 +1,35 @@
-﻿using ToursLib.Models;
+﻿using BigStarLib.Models;
+using ToursLib.Models;
 
 namespace DataDepot {
 	internal class Program {
 		static void Main(string[] args) {
 
+			// LINQ to Objects
+			var colors = new List<string>{ "Green", "Blush", "Yellow",  "Red", "Orange", "Burgundy","Purple", "White", "Black", "Blue" ,"Bronze"};
+			var firstColor = colors.First();
+			Console.WriteLine(firstColor);
 
 			var db1 = new NorthwindLib.Models.NorthwindDbContext();
-			Console.WriteLine(db1.Shippers.First().CompanyName);
+			var firstShipper = db1.Shippers.First();
+			Console.WriteLine(firstShipper.CompanyName);
 
 			var db2 = new ToursDbContext();
-
-			Console.WriteLine(db2.TravelTips.First().Headline);
+			var tip = db2.TravelTips.ElementAt(2);
+			Console.WriteLine(tip.Headline);
 
 			var db3 = new BigStarLib.Models.BigStarDbContext();
 
+			var cards = from c in db3.CollectibleCards
+									where c.TeamName == "BlueShadows"
+									orderby c.CardName
+									select c;
 
-			Console.WriteLine(db3.Aliens.First().Planet);
+			foreach (var card in cards)
+			{
+
+				Console.WriteLine("  " + card.CardName);
+			}
 		}
 	}
 }
