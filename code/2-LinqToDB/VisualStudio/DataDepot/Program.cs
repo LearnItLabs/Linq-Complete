2532cdf94f3c2a345105bd1b015afdd55d4ebe12
Walt Ritscher <waltritscher@outlook.com>
2025-03-15 16:47:21 -0700

    update code examples

--- code/2-LinqToDB/VisualStudio/DataDepot/Program.cs
+++ code/2-LinqToDB/VisualStudio/DataDepot/Program.cs
@@ -10,16 +10,21 @@ namespace DataDepot {
 			var firstColor = colors.First();
 			Console.WriteLine(firstColor);
 
+			// LINQ to Entities
 			var db1 = new NorthwindLib.Models.NorthwindDbContext();
 			var firstShipper = db1.Shippers.First();
 			Console.WriteLine(firstShipper.CompanyName);
 
 			var db2 = new ToursDbContext();
-			var tip = db2.TravelTips.ElementAt(2);
+			var color2 = colors.ElementAt(3);
+			var tip = db2.TravelTips.ElementAt(3);
 			Console.WriteLine(tip.Headline);
 
 			var db3 = new BigStarLib.Models.BigStarDbContext();
-
+			var bColors = from c in colors
+										where c.StartsWith("B")
+										orderby c
+										select c;
 			var cards = from c in db3.CollectibleCards
 									where c.TeamName == "BlueShadows"
 									orderby c.CardName
